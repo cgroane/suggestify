6bf5d6220a81b21ca8374dee80b7bad81ec07bcd
Connor <connorgroane@utexas.edu>
2024-02-28 12:28:58 -0800

    add carousel for text intro

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from '../components/Carousel';
 
 const SignUp = () => {
   
@@ -12,10 +13,7 @@ const SignUp = () => {
   <div className="hero-content text-center">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">Welcome to suggestify</h1>
-      <p className="py-6">This is an app built with Node, React (although only a little), Express, Firebase, Twilio, and the Spotify Web API</p>
-      <p className="py-6">The goal is to enable users to text a number with a link to a song from Spotify and add that song to someone else's playlist</p>
-      <p className="py-6">When you click the button below, you will be directed to authorize this application with Spotify</p>
-      <p className="py-6">After you have authorized Suggestify, it will create a playlist for you in it's own name and will allow songs to be added to that playlist via text</p>
+      <Carousel/>
       <button onClick={submitUserForm} className="btn btn-primary">Get Started</button>
     </div>
   </div>
