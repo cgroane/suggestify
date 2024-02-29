c32cc72892a7eb0bcaf97c1cfcafd639139c9733
Connor <connorgroane@utexas.edu>
2024-02-29 14:06:51 -0800

    link with href

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,18 +3,13 @@ import Carousel from '../components/Carousel';
 
 const SignUp = () => {
   
-  const submitUserForm = (e: React.SyntheticEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    // need to ask for permission to access spotify FIRST, then try to create the account in firebase
-    window.location.pathname = `/api/spotify-login`;
-  }
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content text-center">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">Welcome to suggestify</h1>
       <Carousel/>
-      <button onClick={submitUserForm} className="btn btn-primary">Get Started</button>
+      <a href={`${process.env.REACT_APP_API_URL}/spotify-login`} className="btn btn-primary">Get Started</a>
     </div>
   </div>
 </div>
