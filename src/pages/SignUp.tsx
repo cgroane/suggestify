8f23c263bcc097bd620925ff6378df27907e6ff0
Connor <connorgroane@utexas.edu>
2024-02-29 13:48:40 -0800

    update port var

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -6,7 +6,7 @@ const SignUp = () => {
   const submitUserForm = (e: React.SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault();
     // need to ask for permission to access spotify FIRST, then try to create the account in firebase
-    window.location.href = "http://localhost:3001/api/spotify-login";
+    window.location.href = `${process.env.REACT_APP_API_URL}/spotify-login`;
   }
   return (
     <div className="hero min-h-screen bg-base-200">
