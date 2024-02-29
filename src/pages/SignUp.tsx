b0d9c76098b83413d838bd70d5ea14b956524dbc
Connor <connorgroane@utexas.edu>
2024-02-29 13:59:59 -0800

    use pathname instead of changing href

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -6,7 +6,7 @@ const SignUp = () => {
   const submitUserForm = (e: React.SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault();
     // need to ask for permission to access spotify FIRST, then try to create the account in firebase
-    window.location.href = `${process.env.REACT_APP_API_URL}/spotify-login`;
+    window.location.pathname = `/api/spotify-login`;
   }
   return (
     <div className="hero min-h-screen bg-base-200">
